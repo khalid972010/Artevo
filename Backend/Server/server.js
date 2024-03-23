0fa71240b6ff59cb103a9d36cc3d78e3d4522c5d
mahmouddhamdy <68087837+mahmouddhamdy@users.noreply.github.com>
2024-03-23 18:04:36 +0200

    Rename Utils Folder + Update Client Controller

--- Backend/Server/server.js
+++ Backend/Server/server.js
@@ -4,10 +4,16 @@ const app = express();
 const PORT = process.env.PORT || 7005;
 
 // Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
 app.use("/api/users", UserRoute);
+
+//#region Client Routes
+const clientRoutes = require("./Routes/clientRoutes");
+app.use("/api/clients", clientRoutes);
+//#endregion
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
