b2ab7dad46a4104d25d486de470dd44b8d2dc8ee
mahmouddhamdy <68087837+mahmouddhamdy@users.noreply.github.com>
2024-03-24 22:42:44 +0200

    Upload orderModel

---
+++ Backend/Models/orderModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://khalid972010:khalid123@clusteriti.w66tmq7.mongodb.net/ArtLance"
+);
+const orderSchema = new mongoose.Schema(
+  {
+    date: { type: Date, default: Date.now },
+    from: { type: mongoose.Schema.Types.ObjectId, required: true },
+    to: { type: mongoose.Schema.Types.ObjectId, required: true },
+    state: {
+      type: String,
+      enum: [Pending, inProgress, Refused, Completed],
+      required: true,
+    },
+  },
+  { versionKey: false }
+);
+const Orders = mongoose.model("Orders", orderSchema);
+
+module.exports = Orders;
