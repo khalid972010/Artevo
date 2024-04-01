4fde0c75c44756778f9cb920363895f80acc889c
Khalid Elsayed <khalid972010@gmail.com>
2024-04-01 13:53:01 +0200

    categories card design

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { FooterComponent } from './components/footer/footer.component';
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'front-end';
