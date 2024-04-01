f0e1a4ff4c3e3786e3d904d6e74a50c6ae2d8241
Khalid Elsayed <khalid972010@gmail.com>
2024-04-01 14:20:57 +0200

    fixed popup

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -6,14 +6,16 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { EnterMailComponent } from './components/auth/enter-mail/enter-mail.component';
 import { FreelancerListComponent } from './components/freelancer-list/freelancer-list.component';
-import { FreelancerCardComponent } from './components/freelancer-card/freelancer-card.component';
+
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: "", component: MainComponent},
-  { path: "about", component: AboutUsComponent },
-  { path: "portfolio-list", component: PortfolioListComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  {path: "login/reset/mail", component: EnterMailComponent},
-  {path: "freelancers", component: FreelancerListComponent},
+  { path: '', component: MainComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'portfolio-list', component: PortfolioListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login/reset/mail', component: EnterMailComponent },
+  { path: 'freelancers', component: FreelancerListComponent },
 ];
