31eb586769c65fe9c6e9d620af8c6125ebb92279
SalahNageh <157980270+SalahNageh@users.noreply.github.com>
2024-04-03 01:07:37 +0200

    popup

--- front-end/src/app/components/image-modal/image-modal.component.ts
+++ front-end/src/app/components/image-modal/image-modal.component.ts
@@ -1,20 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FreelancerService } from '../../services/free.lancer';
 
 @Component({
   selector: 'app-image-modal',
   templateUrl: './image-modal.component.html',
-  styleUrls: ['./image-modal.component.css']
+  styleUrls: ['./image-modal.component.css'],
 })
 export class ImageModalComponent {
-
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: { imageUrl: string , freelancer:any},
+    @Inject(MAT_DIALOG_DATA) public data: { imageUrl: string },
     public dialogRef: MatDialogRef<ImageModalComponent>
-  ) { }
+  ) {}
 
   closeModal(): void {
     this.dialogRef.close();
   }
-
 }
