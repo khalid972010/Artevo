340fde485b4efbfe714846e81753d1e9dc8005ed
MazenAymann <mazenaymanmo22@gmail.com>
2024-04-03 14:39:37 +0200

    Modify navBar

--- front-end/src/app/components/navbar/navbar.component.ts
+++ front-end/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 import { FreelancerService } from '../../services/freelancer.service';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
 
@@ -14,11 +13,10 @@ import { SearchbarComponent } from '../searchbar/searchbar.component';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
+
 export class NavbarComponent {
   constructor(
     private router: Router,
-    private http: HttpClient,
-    private freelancerService: FreelancerService
   ) {}
 
   isMenuOpen: boolean = false;
