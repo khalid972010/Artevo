d936ca49eced0773fce933418b19b3a626aa4da5
mahmouddhamdy <68087837+mahmouddhamdy@users.noreply.github.com>
2024-04-15 00:59:16 +0200

    Fix bugs

--- Frontend/src/app/components/add-post/add-post.component.ts
+++ Frontend/src/app/components/add-post/add-post.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input ,OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PortfolioService } from '../../services/portfolio.service';
 import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
@@ -10,94 +10,96 @@ import { FileUploadService } from '../../services/file_upload.service';
 @Component({
   selector: 'app-add-post',
   standalone: true,
-  imports: [FormsModule,
-  CommonModule,
-ReactiveFormsModule],
-  providers:[PortfolioService, FileUploadService],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
+  providers: [PortfolioService, FileUploadService],
   templateUrl: './add-post.component.html',
-  styleUrl: './add-post.component.css'
+  styleUrl: './add-post.component.css',
 })
 export class AddPostComponent implements OnInit {
   private imageLink?: string;
-  constructor(private portfolioService:PortfolioService ,private fb: FormBuilder,private route:ActivatedRoute, private fileUploadService: FileUploadService){}
+  constructor(
+    private portfolioService: PortfolioService,
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private fileUploadService: FileUploadService
+  ) {}
   @Input() imageUrl?: string;
-  selectedCategory:any;
-  description:string="";
-  freelancerId:any;
-  SelectedCategory(e:any)
-  {
+  selectedCategory: any;
+  description: string = '';
+  freelancerId: any;
+  SelectedCategory(e: any) {
     console.log(e.target.value);
-    this.selectedCategory=e.target.value+"";
+    this.selectedCategory = e.target.value + '';
   }
   TechnologyForm: FormGroup = new FormGroup({
-    skills: new FormArray([new FormControl('')])
+    skills: new FormArray([new FormControl('')]),
   });
 
   ngOnInit(): void {
     this.TechnologyForm = this.fb.group({
-      Technology: this.fb.array([this.fb.control('')])
+      Technology: this.fb.array([this.fb.control('')]),
     });
 
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.freelancerId = params['id'];
-      console.log( this.freelancerId);
-          });
+      console.log(this.freelancerId);
+    });
   }
-
 
   NavigateBack() {
-  window.history.back();
+    window.history.back();
   }
   handleFileInput(event: any) {
-  const file = event.target.files[0];
-  const reader = new FileReader();
-  reader.onload = async (e: any) => {
-    // this.imageUrl = e.target.result;
-      this.imageUrl = "https://i.gifer.com/YlWC.gif";
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = async (e: any) => {
+      // this.imageUrl = e.target.result;
+      this.imageUrl = 'https://i.gifer.com/YlWC.gif';
       if (this.imageUrl !== null) {
-        this.imageLink = await this.fileUploadService.uploadAndSaveToDatabase(file);
+        this.imageLink = await this.fileUploadService.uploadAndSaveToDatabase(
+          file
+        );
       }
       this.imageUrl = this.imageLink;
-  };
-  reader.readAsDataURL(file);
-}
-submit(){
-  const currentDate: Date = new Date();
+    };
+    reader.readAsDataURL(file);
+  }
+  submit() {
+    const currentDate: Date = new Date();
 
-  const formattedDate: string = currentDate.toISOString().split('T')[0];
+    const formattedDate: string = currentDate.toISOString().split('T')[0];
 
+    this.portfolioService
+      .AddPortfolio({
+        date: formattedDate,
+        ownerID: this.freelancerId,
+        photos: [this.imageLink],
+        description: this.description,
+        type: this.selectedCategory,
+        technologies: this.TechnologyForm.value.Technology,
+        likesCount: 0,
+        likes: [],
+      })
+      .subscribe(
+        (res) => {
+          console.log(res);
+          window.history.back();
+        },
+        (error) => {}
+      );
+  }
 
+  get TechnologyControls() {
+    return (this.TechnologyForm.get('Technology') as FormArray).controls;
+  }
 
-  this.portfolioService.AddPortfolio({ "date":formattedDate,
-                                       "ownerID":this.freelancerId,
-                                       "photos":[this.imageLink],
-                                       "description":this.description,
-                                       "type":this.selectedCategory,
-                                       "technologies":this.TechnologyForm.value.Technology,
-                                       "likesCount": 0,
-                                       "likes": [],
-                                      }).subscribe(
-    (res) => {
+  addTechnology() {
+    (this.TechnologyForm.get('Technology') as FormArray).push(
+      this.fb.control('')
+    );
+  }
 
-      console.log(res);
-    },
-    (error) => {
-
-    }
- );
- window.history.back();
-}
-
-
-get TechnologyControls() {
-  return (this.TechnologyForm.get('Technology') as FormArray).controls;
-}
-
-addTechnology() {
-  (this.TechnologyForm.get('Technology') as FormArray).push(this.fb.control(''));
-}
-
-removeTechnology(index: number) {
-  (this.TechnologyForm.get('Technology') as FormArray).removeAt(index);
-}
+  removeTechnology(index: number) {
+    (this.TechnologyForm.get('Technology') as FormArray).removeAt(index);
+  }
 }
--- Frontend/src/app/components/profile-freelancer-update/profile-freelancer-update.component.ts
+++ Frontend/src/app/components/profile-freelancer-update/profile-freelancer-update.component.ts
@@ -1,142 +1,140 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FreelancerService } from '../../services/freelancer.service';
 import { UserService } from '../../services/user.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TokenService } from '../../services/token.service';
 import { FileUploadService } from '../../services/file_upload.service';
-import { LoadingComponent } from "../../loading/loading.component";
+import { LoadingComponent } from '../../loading/loading.component';
 
 @Component({
-    selector: 'app-profile-freelancer-update',
-    standalone: true,
-    providers: [FreelancerService, UserService, FileUploadService],
-    templateUrl: './profile-freelancer-update.component.html',
-    styleUrl: './profile-freelancer-update.component.css',
-    imports: [ReactiveFormsModule, LoadingComponent, FormsModule]
+  selector: 'app-profile-freelancer-update',
+  standalone: true,
+  providers: [FreelancerService, UserService, FileUploadService],
+  templateUrl: './profile-freelancer-update.component.html',
+  styleUrl: './profile-freelancer-update.component.css',
+  imports: [ReactiveFormsModule, LoadingComponent, FormsModule],
 })
 export class ProfileFreelancerUpdateComponent implements OnInit {
-  freelancerId:any;
-  freelancer:any;
-  firstName:string="";
-  userName:string="";
+  freelancerId: any;
+  freelancer: any;
+  firstName: string = '';
+  userName: string = '';
   imageUrl?: string;
   coverUrl?: string;
   isLoaded?: boolean = true;
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private freelancerService: FreelancerService,
     private userService: UserService,
     private tokenService: TokenService,
-    private fileUploadService: FileUploadService,
-  ) { }
+    private fileUploadService: FileUploadService
+  ) {}
 
   NavigateBack() {
-  window.history.back();
+    window.history.back();
   }
   goBackAndRefresh(): void {
     window.history.back();
     setTimeout(() => {
       window.location.reload(); // Reload the page after a small delay
-    }, 100); // Adjust the delay as needed
+    }, 500); // Adjust the delay as needed
   }
 
   imageLink = '';
   coverLink = '';
   handleProfileInput(event: any) {
-  const file = event.target.files[0];
-  const reader = new FileReader();
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
     reader.onload = async (e: any) => {
-
       // this.imageUrl = e.target.result;
-      this.imageUrl = "https://i.gifer.com/YlWC.gif";
+      this.imageUrl = 'https://i.gifer.com/YlWC.gif';
       if (this.imageUrl !== null) {
-        this.imageLink = await this.fileUploadService.uploadAndSaveToDatabase(file);
+        this.imageLink = await this.fileUploadService.uploadAndSaveToDatabase(
+          file
+        );
         this.freelancer.profilePicture = this.imageLink;
       }
       this.imageUrl = this.imageLink;
-};
+    };
     reader.readAsDataURL(file);
   }
 
   handleCoverInput(event: any) {
-  const file = event.target.files[0];
-  const reader = new FileReader();
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
     reader.onload = async (e: any) => {
-
-      this.isLoaded = false
+      this.isLoaded = false;
 
       // this.imageUrl = e.target.result;
-      this.coverUrl = "https://i.gifer.com/YlWC.gif";
+      this.coverUrl = 'https://i.gifer.com/YlWC.gif';
       if (this.coverUrl !== null) {
-        this.coverLink = await this.fileUploadService.uploadAndSaveToDatabase(file);
+        this.coverLink = await this.fileUploadService.uploadAndSaveToDatabase(
+          file
+        );
         this.freelancer.coverPicture = this.coverLink;
       }
 
       this.coverUrl = this.coverLink;
       setTimeout(() => {
         this.isLoaded = true; // Reload the page after a small delay
-    }, 2000);
-};
+      }, 2000);
+    };
     reader.readAsDataURL(file);
-}
+  }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.freelancerId = params['id'];
-
-        });
+    });
     this.freelancerService.getFreelancerByID(this.freelancerId).subscribe(
-      (res)=>{
+      (res) => {
         this.freelancer = res.data;
-        this.imageUrl = this.freelancer?.profilePicture
-        this.coverUrl = this.freelancer?.coverPicture
-
+        this.imageUrl = this.freelancer?.profilePicture;
+        this.coverUrl = this.freelancer?.coverPicture;
       },
-      (error)=>{}
-    )
-}
-
-saveChanges(
-  firstName: string,
-  lastName: string,
-  userName:string,
-  password: string,
-  location: string,
-  email: string,
-  about: string,
-
-): void {
-  var obj = {
-    "_id": this.freelancerId,
-    "fullName": firstName + " " + lastName,
-    "userName": userName,
-    "profilePicture": this.freelancer.profilePicture,
-    "coverPicture": this.freelancer.coverPicture,
-    "location": location,
-    "email": email,
-    "about": about
-  };
-
-   if (password !== "") {
-    Object.assign(obj, { "password": password });
+      (error) => {}
+    );
   }
 
- this.userService.UpdateUser({"user":obj,"type":"Freelancer"}).subscribe(
-  (res)=>{
-    console.log(res);
-  },
-  (error)=>{
-    console.log(error);
+  saveChanges(
+    firstName: string,
+    lastName: string,
+    userName: string,
+    password: string,
+    location: string,
+    email: string,
+    about: string
+  ): void {
+    var obj = {
+      _id: this.freelancerId,
+      fullName: firstName + ' ' + lastName,
+      userName: userName,
+      profilePicture: this.freelancer.profilePicture,
+      coverPicture: this.freelancer.coverPicture,
+      location: location,
+      email: email,
+      about: about,
+    };
+
+    if (password !== '') {
+      Object.assign(obj, { password: password });
+    }
+
+    this.userService.UpdateUser({ user: obj, type: 'Freelancer' }).subscribe({
+      next: (res) => {
+        console.log(res);
+        let updatedUser = { ...this.tokenService.getUser(), ...obj };
+        console.log(updatedUser);
+        this.tokenService.setUser(updatedUser);
+        this.goBackAndRefresh();
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
-  )
-
-  this.tokenService.setUser(obj);
-
-  this.goBackAndRefresh();
-  }
-
-
 }
